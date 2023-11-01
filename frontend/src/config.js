5bde979c5e26f9da04423694be462afa5b8d0046
Sanjeev Kumar <129384708+snjkumar23@users.noreply.github.com>
2023-11-01 20:20:01 +0530

    reset config in main

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const CLOUDFRONT_URL = "d358bcbpc13czg.cloudfront.net"
-export const API_GW_URL = "https://hqv2gr7l65.execute-api.us-west-2.amazonaws.com/prod"
+export const CLOUDFRONT_URL = ""
+export const API_GW_URL = ""
